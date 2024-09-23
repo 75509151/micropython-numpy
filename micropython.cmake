22d539e968d237145ab4ccb09a9529e8d6276fa7
jay <75509151@qq.com>
2024-09-23 11:43:19 +0800

    fix cmake

--- micropython.cmake
+++ micropython.cmake
@@ -1,9 +1,9 @@
 add_library(usermod_numpy INTERFACE)
 
-file(GLOB_RECURSE ULAB_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
+file(GLOB_RECURSE NUMPY_SOURCES ${CMAKE_CURRENT_LIST_DIR}/*.c)
 
 target_sources(usermod_numpy INTERFACE
-    ${ULAB_SOURCES}
+    ${NUMPY_SOURCES}
 )
 
 target_include_directories(usermod_numpy INTERFACE
@@ -11,7 +11,7 @@ target_include_directories(usermod_numpy INTERFACE
 )
 
 target_compile_definitions(usermod_numpy INTERFACE
-    MODULE_ULAB_ENABLED=1
+    MODULE_NUMPY_ENABLED=1
 )
 
 target_link_libraries(usermod INTERFACE usermod_numpy)
